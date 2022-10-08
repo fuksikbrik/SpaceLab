ed244e943a9a3ee847a29be2fc7be1ede5ae6296
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2022-10-09 00:57:58 +0300

    Add files via upload

---
+++ SpaceLab/js/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#button');
+const content = document.querySelector('#content');
+
+button.addEventListener("click", function() {
+
+    content.classList.toggle('content-hidden');
+});
